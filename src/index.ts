14099aa51e003646b992c9e6cbeff9528d9247ab
Noortheen Raja <jnoortheen@gmail.com>
2020-10-14 14:19:02 +0530

    chore: configure prettier
    use pre-commit hooks to format

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Copyright (c) Noortheen Raja NJ <jnoortheen@gmail.com>. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ExtensionContext, languages, IndentAction } from 'vscode';
+import { ExtensionContext, languages, IndentAction } from 'vscode'
 
-export function activate(_context: ExtensionContext): any {
-	languages.setLanguageConfiguration('python', {
-		onEnterRules: [
-			{
-				beforeText: /^\s*(?:def|class|for|if|elif|else|while|try|with|finally|except|async).*?:\s*$/,
-				action: { indentAction: IndentAction.Indent }
-			}
-		]
-	});
+import { workspace, Disposable, ExtensionContext } from 'vscode'
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    SettingMonitor,
+    ServerOptions,
+    ErrorAction,
+    ErrorHandler,
+    CloseAction,
+    TransportKind,
+} from 'vscode-languageclient'
+
+export function activate(ctx: ExtensionContext): any {
+    const executable = workspace
+        .getConfiguration('pyls')
+        .get<string>('executable')
+
+    languages.setLanguageConfiguration('python', {
+        onEnterRules: [
+            {
+                beforeText: /^\s*(?:def|class|for|if|elif|else|while|try|with|finally|except|async).*?:\s*$/,
+                action: { indentAction: IndentAction.Indent },
+            },
+        ],
+    })
 }
